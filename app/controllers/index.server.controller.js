a5efafaf3dbd8aa3072eb36ec8f6b950c289a273
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2015-11-20 05:25:43 +0100

    2015-11-20 ejs in action

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,4 +1,10 @@
 //funkcje middleware umieszczamy w kontrolerze
+
+//używamy szablonu ejs o nazwie index (index.ejs) i przekazujemy obiekt zawierający zmienne szablonu
+//ejs będzie szukał szablonu w folderze views
+//w aplikacjach MEAN większość renderowania HTML ma miejsce w angularJs
 exports.render = function(req, res) {
-    res.send('Hello World');
+    res.render('index', {
+        title: 'Hello World'
+    });
 };
